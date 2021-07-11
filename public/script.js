8cfd4f3609bb18be3e25ec5c696ade0b370d964d
deshmukhpalak04@gmail.com <deshmukhpalak04@gmail.com>
2021-07-11 12:19:49 +0530

    ..

---
+++ public/script.js
@@ -0,0 +1,68 @@
+const socket = io('http://localhost:4000')  //import socket.io
+
+const messageContainer = document.getElementById('message-container')
+const roomContainer = document.getElementById('room-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+//user enter his/her name so we use prompt
+
+if (messageForm != null) {
+  const name = prompt('What is your name?')
+  appendMessage('You joined','left')
+  socket.emit('new-user', roomName, name)
+
+  messageForm.addEventListener('submit', e => {
+    e.preventDefault() //it prevent the page form reload
+    const message = messageInput.value
+    appendMessage(`You: ${message}`,'right')
+    socket.emit('send-chat-message', roomName, message) //emit the event send-chat-message with roomName and message
+    messageInput.value = ''
+  })
+}
+
+// here we define the room-created event, when user created a new room 
+socket.on('room-created', room => {
+  const roomElement = document.createElement('div')
+  roomElement.innerText = room
+  const roomLink = document.createElement('a')
+  roomLink.href = `/${room}`
+  roomLink.innerText = 'join'
+  roomContainer.append(roomElement)
+  roomContainer.append(roomLink)
+})
+
+// here we call chat-message event we call appendMessage function
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`,'left')
+})
+
+// here we call user-connected event we call appendMessage function
+socket.on('user-connected', name => {
+  appendMessage(`${name} connected`,'left')
+})
+
+// when user-disconnected this event call
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} disconnected`,'left')
+})
+
+
+function appendMessage(message,position) {
+  const messageElement = document.createElement('div')
+  messageElement.classList.add('message');
+  messageElement.classList.add(position);
+  // messageElement.innerText = message;
+  var d=new Date();
+  var hour=d.getHours();
+  var min=d.getMinutes();
+  // messageElement.innerText = message;
+  
+  messageElement.insertAdjacentHTML('afterbegin',`<p>${message}</p>`);
+  messageContainer.append(messageElement);
+  
+}
+
+
+  
+  
---
+++ server.js
@@ -0,0 +1,69 @@
+//importing all apis
+
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+app.set('views', './views') // use to connect ejs files in views directory
+
+app.set('view engine', 'ejs') // change default template engine to ejs
+app.use(express.static('public')) //connect all static files in public folder
+app.use(express.urlencoded({ extended: true }))  //The express.urlencoded() function is a built-in middleware function in Express. It parses incoming requests with urlencoded payloads and is based on body-parser.
+
+const rooms = { }
+
+
+app.get('/', (req, res) => {
+  res.render('index', { rooms: rooms })
+})
+
+// when post request of '/room'is generated and if the entered room is already present then redirect user to that room else create a new room.
+app.post('/room', (req, res) => {
+  if (rooms[req.body.room] != null) {
+    return res.redirect('/')
+  }
+  rooms[req.body.room] = { users: {} }
+  res.redirect(req.body.room)
+  // Send message that new room was created
+  io.emit('room-created', req.body.room)
+})
+
+//when user created a new room
+app.get('/:room', (req, res) => {
+  if (rooms[req.params.room] == null) {
+    return res.redirect('/')
+  }
+  res.render('room', { roomName: req.params.room })
+})
+
+server.listen(4000)
+
+//it makes connection to socket
+io.on('connection', socket => {
+  //when new-user event call then we join room using socket and broadcast it to every other user connected to same room
+  socket.on('new-user', (room, name) => {
+    socket.join(room)
+    rooms[room].users[socket.id] = name
+    socket.to(room).broadcast.emit('user-connected', name)
+  })
+  // this send-chat-message event called when anyone send the message and this event will broadcast that message to everyone 
+  socket.on('send-chat-message', (room, message) => {
+    socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
+  })
+
+  // this event disconnect the user from the room and broadcast it to everyone
+  socket.on('disconnect', () => {
+    getUserRooms(socket).forEach(room => {
+      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+      delete rooms[room].users[socket.id]
+    })
+  })
+})
+
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name)
+    return names
+  }, [])
+}
